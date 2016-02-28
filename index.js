f0408a211fca4610a71548c63245a3e6055b6863
Juan Soto <juan@juansoto.me>
2016-02-27 21:10:16 -0500

    chore: Expose functions in module

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 'use strict'
 
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string')
-  }
-
-  opts = opts || {}
-
-  return 'I am cool'
+module.exports = {
+  actionTest: require('./lib/actionTest'),
+  reducerTest: require('./lib/reducerTest')
 }
